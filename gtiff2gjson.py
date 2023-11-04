9343c2541aa4b80b42c0d0dc700fbee5d1c9c962
Chipskein <abfn0905@gmail.com>
2023-11-04 18:19:53 -0300

    docs:better --help

--- gtiff2gjson.py
+++ gtiff2gjson.py
@@ -20,14 +20,22 @@ try:
         "--min-channel-color",
         nargs='?',
         default=90,
-        help="",
+        help="""
+            É o valor minímo da banda dos canais R G para processar o pixel.
+            Por exemplo se for igual a 90, so serão processados os pixels que possuirem as bandas do canal R ou G >=90.
+            Default é 90,pode ser um inteiro entre 1-254
+        """,
         type=int
     )
     parser.add_argument(
         "--max-distance-between-points",
         nargs='?',
         default=200,
-        help="",
+        help="""
+            Distância máxima em kilometros entre os pontos que tem a mesma cor.
+            Essa distância é usada para a converter vários pontos de uma mesma cor e um poligono.
+            Default é 200
+        """,
         type=int
     )
     args=parser.parse_args()
@@ -36,6 +44,9 @@ try:
         nome_geo_tiff=os.path.splitext(args.raster)[0]
         args.geojson=f"{nome_geo_tiff}.geojson"
     
+    if args.min_channel_color>254 or args.min_channel_color<=0:
+        raise Exception("--min-channel-color has invalid value")
+
     process_raster(
         raster_name=args.raster,
         geojson_name=args.geojson,
